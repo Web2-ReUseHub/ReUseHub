fbe281c2c9bf11594bb33aec8280282ce3065e33
Amr Khayyat <amr.khhayt124@gmail.com>
2026-03-22 19:55:53 +0200

    Add TrendingSection and PostItem components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Login from './user_manegment/Login';
 import IntroSection from './component/IntroSection';
 import ProfilePage from './user_manegment/ProfilePage';
+import TrendingSection from './component/TrendingSection';
 
 function App() {
   return (
     <>
     <IntroSection></IntroSection>
     <ProfilePage></ProfilePage>
+    <TrendingSection></TrendingSection>
     <Login></Login>
     </>
   )
